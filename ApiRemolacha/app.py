0386e22ccebc471f6cad0e798b0facf4efa07287
jgonzalez <jgonzalez@inartecnologias.es>
2021-04-21 12:04:51 +0200

    cambio a TB

--- ApiRemolacha/app.py
+++ ApiRemolacha/app.py
@@ -27,43 +27,72 @@ normalizarTemp=50
 normalizarAir=100
 normalizarSoil=2000
 
-def uploadThingSpeak():
-	
-	while True:
-		try:
-			#get the current timestamp
-			#current date and time
-			now = datetime.now()
-			#date and time format: dd/mm/YYYY H:M:S
-			format = "%d/%m/%Y %H:%M:%S %z%p"
-			#format datetime using strftime() 
-			time1 = now.strftime(format)
-			 
-			logging.info("Formatted DateTime:", time1)
+
+def on_message(client, userdata, message):  # The callback\n for when a PUBLISH message is received from the server.
+	print("Message received", message.payload)  # Print a received msg
+
+def on_subscribe(client, userdata, mid, granted_qos):
+	print("Se ha suscrito uno")
+
+def on_connect(client, userdata, flags, rc):
+	print("Se ha conectado uno: ", client)
+	print("Connected with result code "+str(rc))
+
+def on_disconnect(client, userdata, rc):
+	print("Se ha desconectado uno: ", client)
+	print("Disconnected with result code "+str(rc))
+
+
+def createData(state):
+	s = "{\"temperature\":" + str(state[0]) +","
+	s = s + "\"air\":" + str(state[1])+","
+	s = s + "\"Pressure\":" + str(state[2])+","
+	s = s + "\"CO2\":" + str(state[3])+","
+	s = s + "\"airQuality\":" + str(state[4])+","
+	s = s + "\"lightEnv\":" + str(state[5])+","
+	s = s + "\"tempSoil\":" + str(state[6])+","
+	s = s + "\"soil\":" + str(state[7])+"}"
+	lockThings.release()
+	return s
+
+def uploadTB():
+
+		client = paho.Client('TBTest', clean_session=True, userdata=None, protocol=paho.MQTTv311, transport="tcp")
+		tb_host = "51.178.52.254"
+		tb_port = 1883
+		ACCESS_TOKEN= "RGfyC21EXK8rdmyMHnke"
+		client.on_message = on_message
+		client.on_subscribe = on_subscribe
+		client.on_connect = on_connect
+		client.on_disconnect = on_disconnect
+		conectado = False
+		while conectado!=True:
+			i = 0
+			while i!=10 and conectado!=True:
+				try:
+					client.loop_start()
+					client.connect(tb_host, tb_port, keepalive=60, bind_address="")
+					client.username_pw_set(ACCESS_TOKEN)
+				except:
+					print("No puedo conectarme")
+					i=i+1
+
+
+				else:
+					conectado = True
+					print("Me he conectado")
+			if conectado == False:
+				time.sleep(1000)
+
+		while True:
 			lockThings.acquire()
-			bulk_update = {
-				"write_api_key": "VMUFLH5MHCFIPJEF",
-				"updates": [{
-						"created_at": time1,
-						"field1": state[0],
-						"field2": state[1],
-						"field3": state[2],
-						"field4": state[3],
-						"field5": state[4],
-						"field6": state[5],
-						"field7": state[6],
-						"field8": state[7],
-					}
-				]
-			}
-			lockThings.release()
-			logging.info("Envio json: " + str(bulk_update))
-			x = requests.post(url, json = bulk_update)
-			time.sleep(120)
-		except Error:
-			print(Error)
+			data = createData(state)
+			print(data)
+			client.publish("v1/devices/me/telemetry", data, 1)
+			print("Mensaje enviado")
+			time.sleep(10)
 		
-x = threading.Thread(target=uploadThingSpeak, args=())
+x = threading.Thread(target=uploadTB, args=())
 x.start()
 
 def guardarEstado(state, cambio):
@@ -73,17 +102,17 @@ def guardarEstado(state, cambio):
 	if "air" in cambio:
 		state[1] = cambio["air"]
 	if "soil" in cambio:
-		state[2] = cambio["soil"]
+		state[7] = cambio["soil"]
 	if "airQuality" in cambio:
-		state[3] = cambio["airQuality"]
+		state[4] = cambio["airQuality"]
 	if "CO2" in cambio:
-		state[4] = cambio["CO2"]
+		state[3] = cambio["CO2"]
 	if "lightEnv" in cambio:
 		state[5] = cambio["lightEnv"]
 	if "tempSoil" in cambio:
 		state[6] = cambio["tempSoil"]
 	if "Pressure" in cambio:
-		state[7] = cambio["Pressure"]	
+		state[2] = cambio["Pressure"]	
 	lockThings.release()
 
 @socketio.on('connect')
